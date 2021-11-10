b0113a1f6cc148a2b3d7f97474a7517f67e3671c
Dominik <MDXIV@protonmail.com>
2021-11-10 17:48:29 +0100

    'best_of_all'

--- src/genetics/genetic_algorithm.py
+++ src/genetics/genetic_algorithm.py
@@ -13,6 +13,8 @@ class GeneticAlgorithm:
     def perform_operations(self):
         fitness_table = []
         epoch_table = []
+        best_from_population = []
+        best_temp = []
         obj = Population(self.parameters)
         obj.create_population()
 
@@ -37,10 +39,20 @@ class GeneticAlgorithm:
         for x in range(self.parameters.population_amount):
             obj.save_results_to_txt_file('final_results_binary', obj.population_list[x].x1.individual_coded+' '+obj.population_list[x].x2.individual_coded+'\n', x, self.parameters.population_amount)
             obj.save_results_to_txt_file('final_results_numbers', str(obj.population_list[x].x1.individual_decoded)+' '+str(obj.population_list[x].x2.individual_decoded)+'\n', x, self.parameters.population_amount)
-            obj.save_results_to_txt_file('best_from_epoch_binary', obj.best_from_epoch[x].x1.individual_coded+' '+obj.best_from_epoch[x].x2.individual_coded+'\n', x, self.parameters.population_amount)
-            obj.save_results_to_txt_file('best_from_epoch_numbers', str(obj.best_from_epoch[x].x1.individual_decoded)+ ' ' + str(obj.best_from_epoch[x].x2.individual_decoded) + '\n', x, self.parameters.population_amount)
+            #obj.save_results_to_txt_file('best_from_epoch_binary', obj.best_from_epoch[x].x1.individual_coded+' '+obj.best_from_epoch[x].x2.individual_coded+'\n', x, self.parameters.population_amount)
+            #obj.save_results_to_txt_file('best_from_epoch_numbers', str(obj.best_from_epoch[x].x1.individual_decoded)+ ' ' + str(obj.best_from_epoch[x].x2.individual_decoded) + '\n', x, self.parameters.population_amount)
             fitness_table.append(obj.population_list[x].f_x)
             epoch_table.append(x)
+            best_temp.append([obj.population_list[x].f_x,x])
+        best_temp.sort()
+        print(best_temp)
+        best_index_gen = int(best_temp[0][1])
+        best_from_population_first = obj.population_list[best_index_gen].x1.individual_decoded
+        best_from_population_second = obj.population_list[best_index_gen].x2.individual_decoded
+        obj.save_results_to_txt_file('best_from_epoch_numbers', str(best_from_population_first)+' '+str(best_from_population_second), x, self.parameters.population_amount)
+        best_from_population_first_bin = obj.population_list[best_index_gen].x1.individual_coded
+        best_from_population_second_bin = obj.population_list[best_index_gen].x2.individual_coded
+        obj.save_results_to_txt_file('best_from_epoch_binary', best_from_population_first_bin+' '+best_from_population_second_bin, x, self.parameters.population_amount)
         plt.plot(epoch_table, fitness_table)
         plt.xlabel('epoch')
         plt.ylabel('fitness function')
