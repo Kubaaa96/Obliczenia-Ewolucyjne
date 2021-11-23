47886233c8abcbc5f38f460049e71ea821d4808d
BartlomiejObratanski <b.obratanski@gmail.com>
2021-11-23 17:44:38 +0100

    Update Chromosome structure

--- src/genetics/chromosome.py
+++ src/genetics/chromosome.py
@@ -1,6 +1,6 @@
 from core.parameters import Parameters
 import math as mth
-import random
+import numpy as np
 
 
 class Chromosome:
@@ -8,21 +8,7 @@ class Chromosome:
         self.parameters = parameters
         self.a = self.parameters.begin_range
         self.b = self.parameters.end_range
-        self.m = self.parameters.number_of_bits
-        self.m_length = self.m_length()
+        self.individual_decoded = self.random_value()
 
-        self.individual_coded = self.random_binary()
-        self.individual_decoded = self.decode_x()
-
-    def m_length(self):
-        return mth.ceil(mth.log2((self.b - self.a) * pow(10, self.m)))
-
-    def random_binary(self):
-        c = ""
-        for _ in range(self.m_length):
-            temp = random.randint(0, 1)
-            c += str(temp)
-        return c
-
-    def decode_x(self):
-        return self.a + int(self.individual_coded, 2) * (self.b - self.a) / (pow(2, self.m_length) - 1)
+    def random_value(self):
+        return np.random.uniform(self.a, self.b, 1)
